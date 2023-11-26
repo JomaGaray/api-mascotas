5f3c68326bba9a7ec9bdb6aa67374335e54e556c
jose.garay <josemgaray14@gmail.com>
2023-11-25 22:42:24 -0300

    cambios en el accesor de usuario

--- src/main/java/acceso/AccesoUsuario.java
+++ src/main/java/acceso/AccesoUsuario.java
@@ -1,4 +1,30 @@
 package acceso;
 
-public class AccesoUsuario {
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.transaction.Transactional;
+import modelo.Usuario;
+
+import java.util.List;
+
+public class AccesoUsuario  implements PanacheRepository<Usuario> {
+
+    @Transactional
+    public Usuario crear(boolean adm, String nombre, String email){
+
+        Usuario usuario = new Usuario(adm,nombre,email);
+
+        persistAndFlush(usuario);
+
+        return usuario;
+    }
+
+    public Usuario obtenerPorId(Long id){
+        return find("id", id).firstResult();
+    }
+
+    public List<Usuario> obtenerTodos(){
+        return  listAll();
+    }
+
+
 }
--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,4 +1,71 @@
 package modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario {
+
+    @Id @GeneratedValue
+    Long id;
+    boolean administrador;
+    String nombre;
+    Integer celular;
+    String email;
+    String direccion;
+
+    public Usuario(boolean administrador, String nombre,  String email) {
+        this.administrador = administrador;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Integer celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail_(String email) {
+        this.email= email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
