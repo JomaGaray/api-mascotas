0706d6aa17d8fb00c42a62761dd01f8868f9856d
jose.garay <jose.garay@fluxit.com.ar>
2024-10-19 15:23:04 -0300

    fix: se eliminan entidades viejas

--- src/main/java/mascotas/project/repositories/CompanieroRepository.java
+++
@@ -1,7 +0,0 @@
-package mascotas.project.repositories;
-
-import mascotas.project.entities.Companiero;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompanieroRepository extends CrudRepository<Companiero, Long> {
-}
